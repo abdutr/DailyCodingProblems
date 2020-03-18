ac8b6b7cbe9eba2d7a9cb1e27b0b9d02a09d2bee
abdutr <abdurrahimuysal@hotmail.com>
2020-03-18 12:35:28 +0300

    DCP-Question1-17.03.2020

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+bool solve_problem(int* arr,int size,int target) {
+	map<int, int> deposit;
+	for (int i = 0; i < size; i++) {
+		if (deposit[arr[i]] == 1) return 1;
+		else {
+			deposit[target - arr[i]] = 1;
+		}
+	}
+	return 0;
+}
+
+int main() {
+	int arr[1001];
+	for (int i = 0; i < 1001; i++) {	
+		arr[i] = i * i;
+	}
+	cout << solve_problem(arr, 1001,1998001)<<endl;
+	cout << "Test";
+	return 1;
+}
+
